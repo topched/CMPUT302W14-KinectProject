54ac5a4bd58221c96047697a08c88e755ee8db32
kdshapka <kshapka@ualberta.ca>
2014-03-22 02:19:02 -0600

    added loginWindow

---
+++ LifeCycle/LifeCycle/LoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LifeCycle
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO - this should check in the database of users - for now, dummy users.
+            if (usernameBox.Text == "patient1" && passwordBox.Text == "password")
+            {
+                var newWindow = new MainWindow();
+                newWindow.Show();
+                this.Close();
+            }
+            else if (usernameBox.Text == "clinician" && passwordBox.Text == "password")
+            {
+                // open a different window
+            }
+            else
+            {
+                loginBlock.Foreground = Brushes.Red;
+                loginBlock.Text = "Username and/or password was incorrect.";
+            }
+        }
+    }
+}
